a9e71f8c3494fd152607ece70f4af7fe54e6c32a
mw483 <159742840+mw483@users.noreply.github.com>
2025-06-26 17:46:41 +0900

    Revert to initial

--- particle_yokouchi/C++/PostProcess/src_tmp/function.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTION
-#define FUNCTION
-
-#include "stParticle.h"
-
-int 	sort_compare (const void* A, const void* B);
-bool 	sort_index (const Particle_Index& A, const Particle_Index& B);
-#endif
